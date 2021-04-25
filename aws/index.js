02f1f56da0a2c8ad1f7a5706b3686fce77cf118f
박성혜 <sperocometa@naver.com>
2021-04-26 00:34:33 +0900

    cloud server

--- aws/index.js
+++ aws/index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const app = express();
 const http = require('http').Server(app);
@@ -6,10 +5,10 @@ const mysql = require('mysql');
 const bodyParser = require('body-parser');
 
 var db = mysql.createConnection({
-  host:'localhost',
-  user:'root',
-  password:'thtka13',
-  database:'sosam'
+  host:'',
+  user:'',
+  password:'',
+  database:''
 });
 
 http.listen(3000, function(){
@@ -18,12 +17,16 @@ http.listen(3000, function(){
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: true }))
+
 
 
 app.get('/test', function(req,res){
     db.query('select * from user', function(err, rows, fields){
-        if(err) console.log(err);
+        if(err) {
+        	console.log(err);
+        	res.send(err);
+        }
         console.log(rows);
         res.send(rows);
     })
@@ -32,7 +35,7 @@ app.get('/test', function(req,res){
 app.post('/login', function(req,res){
   let {id, passwd} = req.body;
     db.query(`select * from user where id='${id}', passwd='${passwd}'`, function(err, rows, fields){
-        if(err) 
+        if(err)
         {
             res.send("login db error");
         }
@@ -53,13 +56,13 @@ app.post('/register', function(req,res){
       values('${id}', '${passwd}', '${name}', '${address}', '${phone}', ${age}, '${message}', '${nickname}' )`, function(err, rows, fields){
         if(err) {
           console.log(err);
-          res.send(false);
+          res.send(err);
         }
         else{
           db.query(`insert into energy_amount(amount, id) values(0, '${id}')` ,function(err, rows, fiedls){
             if(err) {
               console.log(err);
-              res.send(false);
+              res.send(err);
           }
           else
             res.send(true);
@@ -70,7 +73,10 @@ app.post('/register', function(req,res){
 
 app.get('/rank', function(req,res){
     db.query(`select e.amount, u.nickname, u.message from energy_amount as e, user as u where e.id = u.id order by e.amount desc limit 10`, function(err, rows, fields){
-        if(err) console.log(err);
+        if(err) {
+        	console.log(err);
+        	res.send(err);
+        }
         res.send(rows);
     })
 });
@@ -79,29 +85,29 @@ app.post('/donate', function(req,res){
     let { id, date, time, energy, donateto} = req.body;
     let uamount = 0;
     energy = Number(energy);
-    db.query(`insert into donate(id, date, time, energy, donateto) 
+    db.query(`insert into donate(id, date, time, energy, donateto)
       values('${id}', '${date}', '${time}', ${energy},  '${donateto}')`, function(err, rows, fields){
         if(err) {
           console.log(err);
-          res.send(false);
+          res.send(err);
         }
         else{
              db.query(`select * from energy_amount where id='${id}'`, function(err, rows, fields){
               if(err)
-                res.send(false);
+                res.send(err);
               else{
                 uamount = rows[0].amount;
                 db.query(`update energy_amount set amount=${uamount-energy} where id='${id}'`, function(err, rows, fields){
                   if(err) {
                     console.log(err);
-                    res.send('update amount err');
+                    res.send(err);
                   }
                   else
                     res.send(true);
                 });
               }
             });
-            
+
         }
   })
 });
@@ -111,22 +117,22 @@ app.post('/discount', function(req,res){
     let { id, date, time, energy, address} = req.body;
     let uamount = 0;
     energy = Number(energy);
-    db.query(`insert into discount(id, date, time, energy, address) 
+    db.query(`insert into discount(id, date, time, energy, address)
       values('${id}', '${date}', '${time}', ${energy},  '${address}')`, function(err, rows, fields){
         if(err) {
           console.log(err);
-          res.send(false);
+          res.send(err);
         }
         else{
           db.query(`select * from energy_amount where id='${id}'`, function(err, rows, fields){
             if(err)
-              res.send(false);
+              res.send(err);
             else{
               uamount = rows[0].amount;
               db.query(`update energy_amount set amount=${uamount-energy} where id='${id}'`, function(err, rows, fields){
                 if(err) {
                   console.log(err);
-                  res.send('update amount err');
+                  res.send(err);
                 }
                 else
                   res.send(true);
@@ -140,27 +146,29 @@ app.post('/discount', function(req,res){
 
 
 app.post('/exercise', function(req,res){
-  let { id, start, end, date, energy, fid} = req.body;
+  console.log(req.body);
+  if(req.body=={}) res.send('.');
+  let { id, startwith, endwith, date, energy, fid} = req.body;
   let uamount = 0;
   energy = Number(energy);
-  db.query(`insert into elec_energy(id, startwith, endwith, date, fid, energy) 
-    values('${id}', '${start}', '${end}', '${date}',  '${fid}',${energy})`, function(err, rows, fields){
+  db.query(`insert into elec_energy(id, startwith, endwith, date, fid, energy)
+    values('${id}', '${startwith}', '${endwith}', '${date}',  '${fid}',${energy})`, function(err, rows, fields){
       if(err) {
         console.log(err);
-        res.send(false);
+        res.send(err);
       }
       else{
         db.query(`select * from energy_amount where id='${id}'`, function(err, rows, fields){
           if(err){
             console.log(err);
-            res.send(false);
+            res.send(err);
           }
           else{
             uamount = rows[0].amount;
             db.query(`update energy_amount set amount=${uamount+energy} where id='${id}'`, function(err, rows, fields){
               if(err) {
                 console.log(err);
-                res.send('update amount err');
+                res.send(err);
               }
               else
                 res.send(true);
