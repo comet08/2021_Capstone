ea37252d556b68519a8e42f66bed7e6e066d8510
A <37640219+wespito@users.noreply.github.com>
2021-12-30 23:15:07 +0900

    Update currentsensor.ino

--- Arduino/currentsensor/currentsensor.ino
+++ Arduino/currentsensor/currentsensor.ino
@@ -37,37 +37,37 @@ void loop() {
 
   if (input == 's'){
     while(input != 'e'){
-  shuntvoltage = 0;
-  busvoltage = 0;
-  current_mA = 0;
-  loadvoltage = 0;
-  power_mW = 0;
+    shuntvoltage = 0;
+    busvoltage = 0;
+    current_mA = 0;
+    loadvoltage = 0;
+    power_mW = 0;
 
-  shuntvoltage = ina219.getShuntVoltage_mV();
-  busvoltage = ina219.getBusVoltage_V();
-  current_mA = ina219.getCurrent_mA();
-  power_mW = ina219.getPower_mW();
-  loadvoltage = busvoltage + (shuntvoltage / 1000);
+    shuntvoltage = ina219.getShuntVoltage_mV();
+    busvoltage = ina219.getBusVoltage_V();
+    current_mA = ina219.getCurrent_mA();
+    power_mW = ina219.getPower_mW();
+    loadvoltage = busvoltage + (shuntvoltage / 1000);
 
-  Serial.print("전력 출력 시작");
-  Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
-  Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
-  Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
-  Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
-  Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
-  Serial.println("전력 출력 끝");
+    Serial.print("전력 출력 시작");
+    Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+    Serial.print("Shunt Voltage: "); Serial.print(shuntvoltage); Serial.println(" mV");
+    Serial.print("Load Voltage:  "); Serial.print(loadvoltage); Serial.println(" V");
+    Serial.print("Current:       "); Serial.print(current_mA); Serial.println(" mA");
+    Serial.print("Power:         "); Serial.print(power_mW); Serial.println(" mW");
+    Serial.println("전력 출력 끝");
 
-  whole_mA += current_mA;
-  lcd.setCursor(0,0);
-  lcd.print("Current:");
-  lcd.print(current_mA);
-  lcd.setCursor(0,1);
-  lcd.print("Whole:");
-  lcd.print(whole_mA);
-  
-  delay(1000);
-  lcd.clear();
-  input = bluetooth.read();
-  }
+    whole_mA += current_mA;
+    lcd.setCursor(0,0);
+    lcd.print("Current:");
+    lcd.print(current_mA);
+    lcd.setCursor(0,1);
+    lcd.print("Whole:");
+    lcd.print(whole_mA);
+    
+    delay(1000);
+    lcd.clear();
+    input = bluetooth.read();
+    }
   }
 }
